e52f389de3a88f7eb2c60c4917387d896121c721
fuerstchristian <fuerstchristian@icloud.com>
2021-04-06 19:40:52 +0200

    commit (comments for Visualization)

--- CSV_Visualization_v2.py
+++ CSV_Visualization_v2.py
@@ -2,8 +2,9 @@ import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 
-df = pd.read_csv('nelder_mead.csv')
+df = pd.read_csv('nelder_mead.csv')     # Read CSV-File
 
+# Define Range of x1, x2 and x3 values
 min_x1 = min(df.get('x1'))
 max_x1 = max(df.get('x1'))
 min_x2 = min(df.get('x2'))
@@ -11,48 +12,56 @@ max_x2 = max(df.get('x2'))
 min_z = min(df.get('f(x1, x2)'))
 max_z = max(df.get('f(x1, x2)'))
 
+# put values of the x1-, x2- and function-column into an array
 opt_x1 = df._get_column_array(0)
 opt_x2 = df._get_column_array(1)
 func = df._get_column_array(4)
 
+# plot the points in a 3d coordinate system
 fig = px.scatter_3d(
     data_frame=df,
     x='x1',
     y='x2',
     z='f(x1, x2)',
     template='ggplot2',
-    title='Nelder Mead Algorithm - Function ' + str(func[1]) + '\t\t\t\t\t\t Optimal values: (' + str(opt_x1[-1]) + ', ' + str(opt_x2[-1]) + ')',
-    animation_frame='Iteration',
-    range_x=[min_x1-0.5, max_x1+0.5],
-    range_y=[min_x2-0.5, max_x2+0.5],
-    range_z=[min_z-0.5, max_z+0.5]
+    title='Nelder Mead Algorithm - Function ' + str(func[1]) + '\t\t\t\t\t\t Optimal values: (' + str(opt_x1[-1]) + ', '
+          + str(opt_x2[-1]) + ')',    # the optimal value is the last value in the CSV-file (position [-1] in the array)
+    animation_frame='Iteration',        # animate the algorithm according to the value in the 'Iteration'-column of the
+                                        # CSV-file
+    range_x=[min_x1-0.5, max_x1+0.5],   # range of the x1-axis
+    range_y=[min_x2-0.5, max_x2+0.5],   # range of the x2-axis
+    range_z=[min_z-0.5, max_z+0.5]      # range of the f(x1, x2)-axis
 )
 
+# set the size of the points to 5
 fig.update_traces(marker=dict(size=5,
                               line=dict(width=2,
                                         color='DarkSlateGrey')),
                   selector=dict(mode='markers'))
-"""
-fig.update_traces(projection_x=0.67, projection_y=0.67, projection_z=0.67,
-                  selector=dict(mode='scatter3d'))
-"""
+
+# update color and depiction of the planes and the grid
 fig.update_layout(scene=dict(
                     xaxis=dict(
+                         nticks=8,
                          backgroundcolor="rgb(200, 200, 230)",
                          gridcolor="white",
                          showbackground=True,
                          zerolinecolor="white",),
                     yaxis=dict(
+                        nticks=8,
                         backgroundcolor="rgb(230, 200,230)",
                         gridcolor="white",
                         showbackground=True,
                         zerolinecolor="white"),
                     zaxis=dict(
+                        nticks=8,
                         backgroundcolor="rgb(230, 230,200)",
                         gridcolor="white",
                         showbackground=True,
                         zerolinecolor="white",),),)
-# fig.update_traces(marker_line=dict(), selector=dict(type='scattercarpet'))
 
+# fix the size of the coordinate system to a cube
+fig.update_layout(scene_aspectmode='cube')
 
+# show the figure
 pio.show(fig)
