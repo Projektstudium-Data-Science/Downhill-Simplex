73c288080ec34069cbe11636773b6e1321a43593
fuerstchristian <80920195+fuerstchristian@users.noreply.github.com>
2021-04-12 15:50:21 +0200

    Add files via upload

---
+++ blender.py
@@ -0,0 +1,13 @@
+import csv
+import bpy
+
+data = []
+
+bpy.ops.object.empty_add(type='ARROWS', radius=5)
+
+with open('/Users/fuerstchristian/nelder_mead.csv') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        # data.append(row)
+        bpy.ops.mesh.primitive_uv_sphere_add(radius=0.1, location=(float(row[0]), float(row[1]), float(row[2])))
