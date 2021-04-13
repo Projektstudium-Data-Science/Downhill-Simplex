9ce93e34a5e453383dc86c2d7a042ba611634574
fuerstchristian <80920195+fuerstchristian@users.noreply.github.com>
2021-04-13 22:00:24 +0200

    Update blender.py
    Added sleep timer

--- blender.py
+++ blender.py
@@ -11,3 +11,4 @@ with open('/Users/fuerstchristian/nelder_mead.csv') as f:
     for row in reader:
         # data.append(row)
         bpy.ops.mesh.primitive_uv_sphere_add(radius=0.1, location=(float(row[0]), float(row[1]), float(row[2])))
+        bpy.ops.wm.redraw_timer(type='DRAW_WIN_SWAP', iterations=10, time_limit=0.2)
