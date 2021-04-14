e3d1a6d7792d578498588be4fba666347e7f760b
fuerstchristian <80920195+fuerstchristian@users.noreply.github.com>
2021-04-14 13:27:38 +0200

    Update blender.py
    Add Function Surface

--- blender.py
+++ blender.py
@@ -3,6 +3,9 @@ import bpy
 
 data = []
 
+# bpy.ops.mesh.primitive_z_function_surface(equation = "(1 - x)**2 + 100 * ((y - (x**2))**2)", div_x = 15, div_y = 15, size_x = 2, size_y = 2) 
+bpy.ops.mesh.primitive_z_function_surface(equation = "(x**2 + y - 11)**2 + (x + y**2 - 7)**2", div_x = 15, div_y = 15, size_x = 10, size_y = 10)
+
 bpy.ops.object.empty_add(type='ARROWS', radius=5)
 
 with open('/Users/fuerstchristian/nelder_mead.csv') as f:
