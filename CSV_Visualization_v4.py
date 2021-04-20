d74fba8594dcbacff3dcc7ca2edb6f5fb6f0669e
fuerstchristian <80920195+fuerstchristian@users.noreply.github.com>
2021-04-20 20:53:18 +0200

    Update CSV_Visualization_v4.py

--- CSV_Visualization_v4.py
+++ CSV_Visualization_v4.py
@@ -1,9 +1,9 @@
 import numpy as np
 import plotly.graph_objs as go
-import pandas as pd
 import csv
 
-#define "play" and "pause" - Buttons
+
+# define "play" and "pause" - Buttons
 def play(frame_duration=800, transition_duration=0):
     return dict(label="Play", method="animate", args=
     [None, {"frame": {"duration": frame_duration, "redraw": True},
@@ -21,15 +21,7 @@ data = []
 x1 = []
 x2 = []
 x3 = []
-
-# Read CSV-File
-df = pd.read_csv('nelder_mead.csv')
-
-# put values of the x1-, x2- and function-column into an array
-opt_x1 = df._get_column_array(0)
-opt_x2 = df._get_column_array(1)
-opt_x3 = df._get_column_array(2)
-func = df._get_column_array(4)
+func = []
 
 # put all values of the csv file into one array
 with open('nelder_mead.csv') as f:
@@ -44,6 +36,7 @@ while i <= (19*4):
     x1.append(float(data[i][0]))
     x2.append(float(data[i][1]))
     x3.append(float(data[i][2]))
+    func.append(data[i][4])
     i += 1
 
 # x, y, z values of the function
@@ -55,7 +48,7 @@ elif func[-1] == 'Rosenbrock':
     z = (1 - x)**2 + 100 * ((y - (x**2))**2)
 
 # plot a surface of the function
-trace = go.Surface(x=x, y=y, z=z,cmax=1, opacity=0.2, cmin=0, showscale=False)
+trace = go.Surface(x=x, y=y, z=z, cmax=1, cmin=0, showscale=False)  # , opacity=0.2
 data = [trace]
 layout = go.Layout(title='3D Surface plot')
 fig = go.Figure(data=data)
@@ -76,8 +69,8 @@ fig.update_layout(
     )),
 
 # add title and optimal value
-fig.update_layout(title='Nelder Mead Algorithm - Function ' + str(func[1]) + '\t\t\t\t\t\t Optimal values: (' + str(opt_x1[-1]) + ', '
-          + str(opt_x2[-1]) + ')',)
+fig.update_layout(title='Nelder Mead Algorithm - Function ' + str(func[-1]) + '\t\t\t\t\t\t Optimal values: (' +
+                        str(x1[-1]) + ', ' + str(x2[-1]) + ')',)
 
 # 20 iteration with 4 points each
 iterations = [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76]
@@ -91,25 +84,25 @@ frames = [go.Frame(data=[go.Mesh3d(
                 [0.5, 'greenyellow'],
                 [1, 'magenta']],
     # Intensity of each vertex, which will be interpolated and color-coded
-    intensity=np.linspace(0, 1, 4, endpoint=True),
+    intensity=np.linspace(0, 1, 6, endpoint=True),
     intensitymode='cell',
     showscale=False)],
 
     traces=[1],
     name=f'frame{k}'
 ) for k in iterations]
+
+frames.append(go.Frame(data=[go.Scatter3d(
+    x=[x1[-1], x1[-1]],
+    y=[x2[-1], x2[-1]],
+    z=[x3[-1], x3[-1]])]))
+
+frames.append(go.Frame(data=[go.Surface(x=x, y=y, z=z, cmax=1, cmin=0, showscale=False)]))
+
 fig.update(frames=frames)
 
 # add play button for animation of the algorithm
 fig.update_layout(updatemenus=[dict(type="buttons", buttons=[play(), pause()])])
-
-
-# add optimal endpoint
-fig.add_scatter3d(
-    x=[opt_x1[-1], opt_x1[-1]],
-    y=[opt_x2[-1], opt_x2[-1]],
-    z=[opt_x3[-1], opt_x3[-1]]
-)
 
 # fix the size of the coordinate system to a cube
 fig.update_layout(scene_aspectmode='cube')
